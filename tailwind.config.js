32d1064ec2fef5d348bcad5e0a47ff073664d172
Yash Bhardwaj <yb@yashbhardwaj.com>
2023-09-09 15:50:46 +0530

    New sync

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'libre-serif': ['"Libre Baskerville"', 'serif'],
+    },
+  },
+  plugins: [],
+}
+}
